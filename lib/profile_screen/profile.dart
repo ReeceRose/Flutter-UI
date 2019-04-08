4020b8043448dd063654d30f710e45446cc67215
reecerose <reecerose18@gmail.com>
2019-04-07 22:44:01 -0400

    Added edit name and log out button

--- lib/profile_screen/profile.dart
+++ lib/profile_screen/profile.dart
@@ -27,16 +27,84 @@ class ProfileScreen extends StatelessWidget {
                   decoration: BoxDecoration(
                     color: Colors.red,
                     shape: BoxShape.circle,
-                    // border: BoxShape.circle,
-                    // borderRadius: BoxShape.circle,
+                    boxShadow: [
+                      BoxShadow(blurRadius: 10.0, color: Colors.black)
+                    ],
                     image: DecorationImage(
-                      image: NetworkImage('https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'),
+                      image: NetworkImage(
+                          'https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'),
                       fit: BoxFit.cover,
                     ),
                   ),
-                )
+                ),
+                SizedBox(
+                  height: 90.0,
+                ),
+                Text(
+                  'John Doe',
+                  style: TextStyle(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Montserrat'),
+                ),
+                SizedBox(
+                  height: 15.0,
+                ),
+                Text(
+                  'Available for hire',
+                  style: TextStyle(
+                      fontSize: 17.0,
+                      fontStyle: FontStyle.italic,
+                      fontFamily: 'Montserrat'),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Container(
+                  height: 30.0,
+                  width: 95.0,
+                  child: Material(
+                    borderRadius: BorderRadius.circular(20.0),
+                    shadowColor: Colors.greenAccent,
+                    color: Colors.green,
+                    elevation: 7.0,
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: Center(
+                        child: Text(
+                          'Edit Name',
+                          style: TextStyle(
+                              color: Colors.white, fontFamily: 'Montserrat'),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Container(
+                  height: 30.0,
+                  width: 95.0,
+                  child: Material(
+                    borderRadius: BorderRadius.circular(20.0),
+                    shadowColor: Colors.redAccent,
+                    color: Colors.red,
+                    elevation: 7.0,
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: Center(
+                        child: Text(
+                          'Log out',
+                          style: TextStyle(
+                              color: Colors.white, fontFamily: 'Montserrat'),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
               ],
-            )
+            ),
           ),
         ],
       ),
