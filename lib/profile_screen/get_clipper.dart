c49f7fa479889e3b089e017860ea61275afc143c
reecerose <reecerose18@gmail.com>
2019-04-07 22:20:04 -0400

    Clipper completed. Loading network image

--- lib/profile_screen/get_clipper.dart
+++ lib/profile_screen/get_clipper.dart
@@ -3,14 +3,15 @@ import 'package:flutter/material.dart';
 class GetClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
-    // TODO: implement getClip
-    return null;
+    var path = new Path();
+    path.lineTo(0.0, size.height / 2.75);
+    path.lineTo(size.width + 650, 0.0);
+    path.close();
+    return path;
   }
 
   @override
   bool shouldReclip(CustomClipper<Path> oldClipper) {
-    // TODO: implement shouldReclip
-    return null;
+    return true;
   }
-
 }
--- lib/profile_screen/profile.dart
+++ lib/profile_screen/profile.dart
@@ -14,7 +14,30 @@ class ProfileScreen extends StatelessWidget {
               color: Colors.black.withOpacity(0.8),
             ),
             clipper: GetClipper(),
-          )
+          ),
+          Positioned(
+            width: 350.0,
+            left: 25.0,
+            top: MediaQuery.of(context).size.height / 5,
+            child: Column(
+              children: <Widget>[
+                Container(
+                  width: 150.0,
+                  height: 150.0,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    shape: BoxShape.circle,
+                    // border: BoxShape.circle,
+                    // borderRadius: BoxShape.circle,
+                    image: DecorationImage(
+                      image: NetworkImage('https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                )
+              ],
+            )
+          ),
         ],
       ),
     );
