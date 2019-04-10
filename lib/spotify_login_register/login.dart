59495d54d4b22085273d5620e8157737b3e41f6b
reecerose <reecerose18@gmail.com>
2019-04-10 18:43:26 -0400

    Added email and password input. Added forgot password

--- lib/spotify_login_register/login.dart
+++ lib/spotify_login_register/login.dart
@@ -4,6 +4,7 @@ class SpotifyLoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomPadding: false,
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
@@ -40,6 +41,59 @@ class SpotifyLoginPage extends StatelessWidget {
               ],
             ),
           ),
+          Container(
+            padding: EdgeInsets.fromLTRB(20.0, 35.0, 20.0, 0.0),
+            child: Column(
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'EMAIL ',
+                    labelStyle: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey,
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'PASSWORD',
+                    labelStyle: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey,
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                  ),
+                  obscureText: true,
+                ),
+                SizedBox(),
+                Container(
+                  alignment: Alignment(1.0, 0.0),
+                  padding: EdgeInsets.fromLTRB(20.0, 15.0, 0.0, 0.0),
+                  child: InkWell(
+                    child: Text(
+                      'Forgot Password',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 3, 216, 90),
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Montserrat',
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
