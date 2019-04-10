7ff3cf72d66b95ffd6726450fa3a5cf0810bff40
reecerose <reecerose18@gmail.com>
2019-04-10 18:31:31 -0400

    Header on login page

--- lib/spotify_login_register/login.dart
+++ lib/spotify_login_register/login.dart
@@ -3,6 +3,45 @@ import 'package:flutter/material.dart';
 class SpotifyLoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
-  }  
-} 
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
+                  child: Text(
+                    'Hello',
+                    style:
+                        TextStyle(fontSize: 80.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(15.0, 175.0, 0.0, 0.0),
+                  child: Text(
+                    'There',
+                    style:
+                        TextStyle(fontSize: 80.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(225.0, 175.0, 0.0, 0.0),
+                  child: Text(
+                    '.',
+                    style: TextStyle(
+                      fontSize: 80.0,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 3, 216, 90),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
