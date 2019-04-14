611a911237e1e2a290d566220b253d672d5a33d5
reecerose <reecerose18@gmail.com>
2019-04-13 22:27:03 -0400

    Work on appbar

---
+++ lib/talent_hire/hire.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HirePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    const Color greenColour = Color.fromARGB(255, 46, 194, 105);
+    const Color backgroundGreen = Color.fromARGB(255, 233, 248, 239);
+    return Scaffold(
+      appBar: new AppBar(
+        title: new IconButton(
+          onPressed: () {},
+          color: greenColour,
+          icon: Icon(Icons.gesture),
+        ),
+        elevation: 0.0,
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.arrow_back),
+          color: Colors.grey,
+        ),
+      ),
+    );
+  }
+}
