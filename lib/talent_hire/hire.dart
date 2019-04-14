41cb208861c0e9ca0cc6038584fbe3873040bf2e
reecerose <reecerose18@gmail.com>
2019-04-13 23:31:09 -0400

    Added my coaches section

--- lib/talent_hire/hire.dart
+++ lib/talent_hire/hire.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class HirePage extends StatelessWidget {
+  Color greenColour = Color.fromARGB(255, 46, 194, 105);
+  Color backgroundGreen = Color.fromARGB(255, 233, 248, 239);
   @override
   Widget build(BuildContext context) {
-    const Color greenColour = Color.fromARGB(255, 46, 194, 105);
-    const Color backgroundGreen = Color.fromARGB(255, 233, 248, 239);
     return Scaffold(
       appBar: new AppBar(
         title: new IconButton(
@@ -128,9 +128,147 @@ class HirePage extends StatelessWidget {
                 ),
               )
             ],
+          ),
+          SizedBox(
+            height: 40.0,
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 25.0, right: 25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'MY COACHES',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontFamily: 'Montserrat',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 12.0,
+                  ),
+                ),
+                Text(
+                  'VIEW PAST SESSIONS',
+                  style: TextStyle(
+                    color: greenColour,
+                    fontFamily: 'Montserrat',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 12.0,
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          GridView.count(
+            primary: false,
+            crossAxisCount: 2,
+            crossAxisSpacing: 2.0,
+            mainAxisSpacing: 4.0,
+            shrinkWrap: true,
+            children: <Widget>[
+              _buildCard('John', 'Available', 1),
+              _buildCard('John', 'Away', 2),
+              _buildCard('John', 'Away', 3),
+              _buildCard('John', 'Available', 4),
+              _buildCard('John', 'Away', 5),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCard(String name, String status, int cardIndex) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      elevation: 7.0,
+      child: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 12.0,
+          ),
+          Stack(
+            children: <Widget>[
+              Container(
+                height: 60.0,
+                width: 60.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30.0),
+                  color: Colors.grey,
+                  image: DecorationImage(
+                    image: NetworkImage(
+                        'https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 40.0),
+                height: 18.0,
+                width: 18.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30.0),
+                  color: status == 'Away' ? Colors.amber : greenColour,
+                  border: Border.all(
+                    color: Colors.white,
+                    style: BorderStyle.solid,
+                    width: 2.0,
+                  ),
+                ),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          Text(
+            name,
+            style: TextStyle(
+              fontFamily: 'Montserrat',
+              fontWeight: FontWeight.bold,
+              fontSize: 24.0,
+              color: Colors.black,
+            ),
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          Text(
+            status,
+            style: TextStyle(
+              fontFamily: 'Montserrat',
+              fontWeight: FontWeight.bold,
+              fontSize: 12.0,
+              color: Colors.grey,
+            ),
+          ),
+          SizedBox(
+            height: 15.0,
+          ),
+          Expanded(
+            child: Container(
+              width: 175.0,
+              decoration: BoxDecoration(
+                color: status == 'Available' ? greenColour : Colors.grey,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(10.0),
+                  bottomRight: Radius.circular(10.0),
+                ),
+              ),
+              child: Center(
+                child: Text('Request'),
+              ),
+            ),
           )
         ],
       ),
+      margin: cardIndex.isEven
+          ? EdgeInsets.fromLTRB(10.0, 0.0, 25.0, 10.0)
+          : EdgeInsets.fromLTRB(25.0, 0.0, 5.0, 10.0),
     );
   }
 }
