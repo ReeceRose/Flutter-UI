71981a9d7fb57b025c3448cbac5a103f6757c5cd
reecerose <reecerose18@gmail.com>
2019-04-13 22:56:36 -0400

    Completed buy more section

--- lib/talent_hire/hire.dart
+++ lib/talent_hire/hire.dart
@@ -14,7 +14,7 @@ class HirePage extends StatelessWidget {
         ),
         elevation: 0.0,
         centerTitle: true,
-        backgroundColor: Colors.transparent,
+        backgroundColor: Colors.white,
         leading: IconButton(
           onPressed: () {},
           icon: Icon(Icons.arrow_back),
@@ -25,6 +25,109 @@ class HirePage extends StatelessWidget {
             onPressed: () {},
             color: Colors.grey,
             icon: Icon(Icons.menu),
+          )
+        ],
+      ),
+      body: ListView(
+        shrinkWrap: true,
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              Container(
+                alignment: Alignment(0.0, -0.4),
+                height: 100,
+                color: Colors.white,
+                child: Text(
+                  'Get coaching',
+                  style: TextStyle(
+                    fontFamily: 'Montserrat',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(25.0, 90.0, 25.0, 0.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20.0),
+                  // boxShadow: [
+                  //   BoxShadow(
+                  //     blurRadius: 0.5,
+                  //     color: Colors.grey,
+                  //   )
+                  // ],
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Stack(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.fromLTRB(25.0, 25.0, 5.0, 5.0),
+                          child: Text(
+                            'YOU HAVE',
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold,
+                              fontSize: 12.0,
+                            ),
+                          ),
+                        ),
+                        Row(
+                          children: <Widget>[
+                            Container(
+                              padding:
+                                  EdgeInsets.fromLTRB(25.0, 40.0, 0.0, 25.0),
+                              child: Text(
+                                '200',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontFamily: 'Montserrat',
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 40.0,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding:
+                                  EdgeInsets.fromLTRB(0.0, 40.0, 0.0, 25.0),
+                              child: Icon(
+                                Icons.favorite,
+                                size: 30.0,
+                                color: Colors.red,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      width: 100,
+                    ),
+                    Container(
+                      height: 50.0,
+                      width: 125.0,
+                      decoration: BoxDecoration(
+                        color: backgroundGreen,
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Buy more',
+                          style: TextStyle(
+                            color: greenColour,
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
           )
         ],
       ),
