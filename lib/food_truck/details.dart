598838e401d89a3fd4a33448642175b44f8164e8
reecerose <reecerose18@gmail.com>
2019-04-14 13:52:27 -0400

    Added main image section

--- lib/food_truck/details.dart
+++ lib/food_truck/details.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'selected_photo.dart';
+
 class FoodTruckDetailsPage extends StatefulWidget {
   @override
   _FoodTruckDetailsPageState createState() => _FoodTruckDetailsPageState();
@@ -15,20 +17,105 @@ class _FoodTruckDetailsPageState extends State<FoodTruckDetailsPage> {
 
   void _previousImage() {
     setState(() {
-     photoIndex = photoIndex > 0 ? photoIndex - 1 : 0; 
+      photoIndex = photoIndex > 0 ? photoIndex - 1 : 0;
     });
   }
 
   void _nextImage() {
     setState(() {
-     photoIndex = photoIndex < photos.length - 1 ? photoIndex + 1 : photoIndex; 
+      photoIndex = photoIndex < photos.length - 1 ? photoIndex + 1 : photoIndex;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    const Color starColour = Color.fromARGB(255, 227, 108, 74);
+    return Scaffold(
+      body: ListView(
+        shrinkWrap: true,
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Stack(
+                children: <Widget>[
+                  Container(
+                    height: 250.0,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(photos[photoIndex]),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.arrow_back_ios),
+                        color: Colors.white,
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.favorite),
+                        color: Color.fromARGB(255, 245, 74, 118),
+                        iconSize: 30.0,
+                      )
+                    ],
+                  ),
+                  Positioned(
+                    top: 220.0,
+                    left: 5.0,
+                    child: Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.star,
+                          color: starColour,
+                        ),
+                        Icon(
+                          Icons.star,
+                          color: starColour,
+                        ),
+                        Icon(
+                          Icons.star,
+                          color: starColour,
+                        ),
+                        Icon(
+                          Icons.star,
+                          color: starColour,
+                        ),
+                        Icon(
+                          Icons.star,
+                          color: Color.fromARGB(255, 123, 145, 158),
+                        ),
+                        SizedBox(
+                          width: 2.0,
+                        ),
+                        Text(
+                          '4.0',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16.0,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 35.0,
+                        ),
+                        SelectedPhoto(
+                          photoIndex: photoIndex,
+                          numberOfDots: photos.length,
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
